fc8aa4601fa5fa476eec5e339e94091d629d4371
lizardgai4 <lizardgai4@gmail.com>
2025-01-22 21:34:56 -0800

    Made more sensical numbers

--- homonyms.go
+++ homonyms.go
@@ -1089,7 +1089,7 @@ func homonymSearch() error {
 
 	defer previous.Close()
 
-	dictCount := uint8(16)
+	dictCount := uint8(4)
 	for i := uint8(0); i < dictCount; i++ {
 		dictArray = append(dictArray, FwewDictInit(i+1))
 	}
@@ -1101,7 +1101,7 @@ func homonymSearch() error {
 	fmt.Println("Stage 3:")
 	// number of dictionaries, minimum affixes, maximum affixes, maximum word length, start at word number N
 	// warn about inefficiencies, nasal assimilation mode
-	StageThree(dictCount, 0, 127, 127, 2700, true, false)
+	StageThree(dictCount, 0, 4, 14, 0, true, false)
 
 	return nil
 }
